1c1ecae74366f645e75b64cf2b4acb2c1f79c73d
Gabo Esquivel <contact@gaboesquivel.com>
2023-03-09 11:37:05 -0600

    release: 2.0.0 React 18 support

--- packages/eosio-hooks-transit/src/TransitProvider.tsx
+++ packages/eosio-hooks-transit/src/TransitProvider.tsx
@@ -1,53 +1,50 @@
-import React from 'react'
-import { TransitProviderProps, LoginParams } from './types'
-import useTransitReducer from './useTransitReducer'
-import { TransitContext } from './TransitContext'
-import { useLocalStorage } from 'react-use'
+import React, { PropsWithChildren } from "react";
+import { TransitProviderProps, LoginParams } from "./types";
+import useTransitReducer from "./useTransitReducer";
+import { TransitContext } from "./TransitContext";
+import { useLocalStorage } from "./useLocalStorage";
 
-export const TransitProvider: React.FC<TransitProviderProps> = ({ children, accessContext, autoLogin = true }) => {
-  const [state, dispatch] = useTransitReducer()
-  const isFirstRender = React.useRef(true)
-  const [activeProviderIndex, setActiveProviderIndex, removeActiveProviderIndex] = useLocalStorage<number>(
-    'active-provider-index',
-    state.activeProviderIndex,
-  )
+export const TransitProvider = ({ children, accessContext, autoLogin = true }: PropsWithChildren<TransitProviderProps>) => {
+  const [state, dispatch] = useTransitReducer();
+  const isFirstRender = React.useRef(true);
+  const [activeProviderIndex, setActiveProviderIndex] = useLocalStorage("active-provider-index", state.activeProviderIndex);
 
   const login = async ({ providerIndex, accountName, authorization }: LoginParams) => {
     try {
-      dispatch({ type: 'LOGIN_START' })
+      dispatch({ type: "LOGIN_START" });
 
-      const selectedProvider = accessContext.getWalletProviders()[providerIndex]
-      if (!selectedProvider) throw new Error('Invalid wallet provider index')
+      const selectedProvider = accessContext.getWalletProviders()[providerIndex];
+      if (!selectedProvider) throw new Error("Invalid wallet provider index");
 
-      const wallet = accessContext.initWallet(selectedProvider)
+      const wallet = accessContext.initWallet(selectedProvider);
 
-      await wallet.connect()
+      await wallet.connect();
 
-      if (accountName && authorization) await wallet.login(accountName, authorization)
-      if (accountName && !authorization) await wallet.login(accountName)
-      if (!accountName && !authorization) await wallet.login()
+      if (accountName && authorization) await wallet.login(accountName, authorization);
+      if (accountName && !authorization) await wallet.login(accountName);
+      if (!accountName && !authorization) await wallet.login();
 
-      setActiveProviderIndex(providerIndex)
-      dispatch({ type: 'LOGIN_SUCCESS', payload: { wallet } })
+      setActiveProviderIndex(providerIndex);
+      dispatch({ type: "LOGIN_SUCCESS", payload: { wallet } });
     } catch (error) {
-      dispatch({ type: 'LOGIN_ERROR', payload: { error } })
+      dispatch({ type: "LOGIN_ERROR", payload: { error } });
     }
-  }
+  };
 
   const logout = async () => {
-    await state.wallet?.logout()
-    removeActiveProviderIndex()
-    dispatch({ type: 'LOGOUT' })
-  }
+    await state.wallet?.logout();
+    setActiveProviderIndex(null);
+    dispatch({ type: "LOGOUT" });
+  };
 
   React.useEffect(() => {
-    if (!isFirstRender.current) return
-    if (!state.accessContext) dispatch({ type: 'SET_ACCESS_CONTEXT', payload: { accessContext } })
-    if (autoLogin && !state.wallet && activeProviderIndex !== undefined) login({ providerIndex: activeProviderIndex })
-    isFirstRender.current = false
-  })
+    if (!isFirstRender.current) return;
+    if (!state.accessContext) dispatch({ type: "SET_ACCESS_CONTEXT", payload: { accessContext } });
+    if (autoLogin && !state.wallet && activeProviderIndex !== undefined) login({ providerIndex: activeProviderIndex });
+    isFirstRender.current = false;
+  });
 
-  return <TransitContext.Provider value={{ ...state, login, logout }}>{children}</TransitContext.Provider>
-}
+  return <TransitContext.Provider value={{ ...state, login, logout }}>{children}</TransitContext.Provider>;
+};
 
-export default TransitProvider
+export default TransitProvider;
